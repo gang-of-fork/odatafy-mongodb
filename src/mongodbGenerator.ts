4ceac325f940ad858d88b2448dbdfa3267885b14
Robin Reyer <robin.reyer@t-online.de>
2024-04-23 16:12:16 +0200

    fix order of pagination and search

--- src/mongodbGenerator.ts
+++ src/mongodbGenerator.ts
@@ -1,24 +1,24 @@
-import url from 'url';
+import url from "url";
 
-import { generateMatchFromFilterExpr } from './filterGenerator';
-import { generateLimitFromTopExpr } from './limitGenerator';
-import { generateSkipFromSkipExpr } from './skipGenerator';
-import { generateSortFromOrderbyExpr } from './sortGenerator';
-import { generateProjectFromSelectExpr } from './selectGenerator';
-import { generateLookupFromExpand, CollectionMap } from './lookupGenerator';
-import { generateComputeStageFromComputedExpr } from './computeGenerator';
-import { generateSearchFromSearchExpr } from './searchGenerator';
+import { generateMatchFromFilterExpr } from "./filterGenerator";
+import { generateLimitFromTopExpr } from "./limitGenerator";
+import { generateSkipFromSkipExpr } from "./skipGenerator";
+import { generateSortFromOrderbyExpr } from "./sortGenerator";
+import { generateProjectFromSelectExpr } from "./selectGenerator";
+import { generateLookupFromExpand, CollectionMap } from "./lookupGenerator";
+import { generateComputeStageFromComputedExpr } from "./computeGenerator";
+import { generateSearchFromSearchExpr } from "./searchGenerator";
 
-import { oDataParameters } from 'odatafy-parser';
-import { Document } from 'mongodb';
+import { oDataParameters } from "odatafy-parser";
+import { Document } from "mongodb";
 
 export type MongoDBODatafyOpts = {
-    expandMapping?: CollectionMap,
-    returnEmptyPipeline?: boolean,
-    regexSearchFields?: string[],
-    returnDataCountQuery?: boolean,
-    returnCountOnly?: boolean
-}
+	expandMapping?: CollectionMap;
+	returnEmptyPipeline?: boolean;
+	regexSearchFields?: string[];
+	returnDataCountQuery?: boolean;
+	returnCountOnly?: boolean;
+};
 
 /**
  * Get MongoDB aggregation query from a given url can be obtained by nodes req.url
@@ -26,25 +26,41 @@ export type MongoDBODatafyOpts = {
  * @param opts options for getting the url
  * @returns MongoDB aggregation query
  */
-export function getQueryFromUrl(oDataUrl: string, opts?: MongoDBODatafyOpts): Document[] {
-    const query = url.parse(oDataUrl, true).query;
-    const validParams = ['filter', 'orderby', 'skip', 'top', 'expand', 'compute', 'select', 'search'];
-    const params = Object.keys(query);
+export function getQueryFromUrl(
+	oDataUrl: string,
+	opts?: MongoDBODatafyOpts
+): Document[] {
+	const query = url.parse(oDataUrl, true).query;
+	const validParams = [
+		"filter",
+		"orderby",
+		"skip",
+		"top",
+		"expand",
+		"compute",
+		"select",
+		"search",
+	];
+	const params = Object.keys(query);
 
-    let parseParameters: oDataParameters = {}
+	let parseParameters: oDataParameters = {};
 
-    validParams.forEach((param: string) => {
-        //check if url 
-        if (params.includes(param) && params.includes('$' + param)) {
-            throw new Error(`Malformed oData url, cannot contain param: ${param} and param: $${param}`)
-        }
+	validParams.forEach((param: string) => {
+		//check if url
+		if (params.includes(param) && params.includes("$" + param)) {
+			throw new Error(
+				`Malformed oData url, cannot contain param: ${param} and param: $${param}`
+			);
+		}
 
-        if (params.includes(param) || params.includes('$' + param)) {
-            parseParameters[param as keyof oDataParameters] = query[(params.includes(param) ? param : '$' + param)] as string
-        }
-    });
+		if (params.includes(param) || params.includes("$" + param)) {
+			parseParameters[param as keyof oDataParameters] = query[
+				params.includes(param) ? param : "$" + param
+			] as string;
+		}
+	});
 
-    return getQuery(parseParameters, opts);
+	return getQuery(parseParameters, opts);
 }
 
 /**
@@ -53,100 +69,110 @@ export function getQueryFromUrl(oDataUrl: string, opts?: MongoDBODatafyOpts): Do
  * @param opts options for getting the url
  * @returns MongoDB aggregaion pipeline
  */
-export function getQuery(parameters: oDataParameters, opts?: MongoDBODatafyOpts): Document[] {
-    const pipeline: Document[] = [];
-    let countPipeline: Document[] = [];
+export function getQuery(
+	parameters: oDataParameters,
+	opts?: MongoDBODatafyOpts
+): Document[] {
+	const pipeline: Document[] = [];
+	let countPipeline: Document[] = [];
 
-    if (parameters.expand) {
-        pipeline.push(...generateLookupFromExpand(parameters.expand, opts?.expandMapping? opts.expandMapping: {}));
-    }
+	if (parameters.expand) {
+		pipeline.push(
+			...generateLookupFromExpand(
+				parameters.expand,
+				opts?.expandMapping ? opts.expandMapping : {}
+			)
+		);
+	}
 
-    if (parameters.compute) {
-        pipeline.push(generateComputeStageFromComputedExpr(parameters.compute));
-    }
+	if (parameters.compute) {
+		pipeline.push(generateComputeStageFromComputedExpr(parameters.compute));
+	}
 
-    if (parameters.filter) {
-        pipeline.push(generateMatchFromFilterExpr(parameters.filter));
-    }
+	if (parameters.filter) {
+		pipeline.push(generateMatchFromFilterExpr(parameters.filter));
+	}
 
-    /* copy to count query */
-    if(opts?.returnDataCountQuery || opts?.returnCountOnly) {
-        countPipeline = [...pipeline];
-    }
+	/* copy to count query */
+	if (opts?.returnDataCountQuery || opts?.returnCountOnly) {
+		countPipeline = [...pipeline];
+	}
 
+	if (parameters.search) {
+		const searchExpr = generateSearchFromSearchExpr(
+			parameters.search,
+			opts?.regexSearchFields
+		);
 
-    if (parameters.orderby) {
-        pipeline.push(generateSortFromOrderbyExpr(parameters.orderby));
-    }
+		pipeline.push(searchExpr);
+		countPipeline.push(searchExpr);
+	}
 
-    if (parameters.skip) {
-        pipeline.push(generateSkipFromSkipExpr(parameters.skip));
-    }
+	if (parameters.skip) {
+		pipeline.push(generateSkipFromSkipExpr(parameters.skip));
+	}
 
-    if (parameters.top) {
-        pipeline.push(generateLimitFromTopExpr(parameters.top));
-    }
+	if (parameters.top) {
+		pipeline.push(generateLimitFromTopExpr(parameters.top));
+	}
 
-    if(parameters.select) {
-        pipeline.push(generateProjectFromSelectExpr(parameters.select));
-    }
+	if (parameters.select) {
+		pipeline.push(generateProjectFromSelectExpr(parameters.select));
+	}
 
-    if(parameters.search) {
-        const searchExpr = generateSearchFromSearchExpr(parameters.search, opts?.regexSearchFields);
+	if (parameters.orderby) {
+		pipeline.push(generateSortFromOrderbyExpr(parameters.orderby));
+	}
 
-        pipeline.push(searchExpr);
-        countPipeline.push(searchExpr);
-    }
+	//add default steps if pipline must not be empty - i.e. in mongoose an empty pipeline returns an error
+	if (!opts?.returnEmptyPipeline && pipeline.length == 0) {
+		pipeline.push(
+			{
+				$addFields: {
+					odatafyMongoDBTempField: "",
+				},
+			},
+			{
+				$project: {
+					odatafyMongoDBTempField: 0,
+				},
+			}
+		);
 
-    //add default steps if pipline must not be empty - i.e. in mongoose an empty pipeline returns an error
-    if(!opts?.returnEmptyPipeline && pipeline.length == 0) {
-        pipeline.push(
-            {
-                $addFields: {
-                    odatafyMongoDBTempField: ""
-                }
-            },
-            {
-                $project: {
-                    odatafyMongoDBTempField: 0
-                }
-            }
-        )
+		countPipeline = [...pipeline];
+	}
 
-        countPipeline = [...pipeline];
-    }
+	countPipeline.push({
+		$count: "count",
+	});
 
-    countPipeline.push({
-        $count: "count"
-    });
+	if (opts?.returnCountOnly) {
+		return countPipeline;
+	}
 
-    if(opts?.returnCountOnly) {
-        return countPipeline
-    }
+	if (opts?.returnDataCountQuery) {
+		return [
+			{
+				$facet: {
+					data: pipeline,
+					countTmp: countPipeline,
+				},
+			},
+			{
+				$addFields: {
+					countTmp2: {
+						$first: "$countTmp",
+					},
+				},
+			},
+			{
+				$project: {
+					data: "$data",
+					count: "$countTmp2.count",
+				},
+			},
+		];
+	}
 
-    if(opts?.returnDataCountQuery) {
-        return [
-            {
-                "$facet": {
-                    "data": pipeline,
-                    "countTmp": countPipeline
-                }
-            },
-            {
-                "$addFields": {
-                    "countTmp2": {
-                        "$first": "$countTmp"
-                    }
-                }
-            },
-            {
-                "$project": {
-                    "data": "$data",
-                    "count": "$countTmp2.count"
-                }
-            }
-        ]
-    }
-
-    return pipeline;
+	return pipeline;
 }
